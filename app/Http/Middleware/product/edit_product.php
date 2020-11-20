03bcd33519e280bd94b3cb0fce02d7745d68d37d
Fahad Khan <fahadkhan1740@outlook.com>
2020-11-20 11:52:51 +0300

    formatted code

--- app/Http/Middleware/product/edit_product.php
+++ app/Http/Middleware/product/edit_product.php
@@ -17,14 +17,13 @@ class edit_product
      */
     public function handle($request, Closure $next)
     {
-      $check =  DB::table('manage_role')
-                 ->where('user_types_id',Auth()->user()->role_id)
-                 ->where('products_update',1)
-                 ->first();
-        if($check == null){
-          return  redirect('not_allowed');
-        }else{
-          return $next($request);
+        $check = DB::table('manage_role')
+            ->where('user_types_id', Auth()->user()->role_id)
+            ->where('products_update', 1)
+            ->first();
+        if ($check == null) {
+            return redirect('not_allowed');
         }
+        return $next($request);
     }
 }
