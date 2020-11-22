4093bad7adcb897609b5be90d50a64cd329bc648
Fahad Khan <fahadkhan1740@outlook.com>
2020-11-22 22:16:03 +0300

    fixed banner images

--- resources/views/web/carousels/boot-carousel-content-with-left-banner.blade.php
+++ resources/views/web/carousels/boot-carousel-content-with-left-banner.blade.php
@@ -1,5 +1,5 @@
 <main id="scrollbar-body" data-scrollbar>
-  
+
     <!-- banner wrap -->
     <section class="main-warp">
       <div class="container">
@@ -8,25 +8,25 @@
             <!-- Wrapper for slides -->
             <div class="carousel-inner">
               <div class="carousel-item active">
-                <figure class="figure-banner" style="background-image: url('web/images/cus/banner-slide-1.jpg');"></figure>
+                <figure class="figure-banner" style="background-image: url('public/web/images/cus/banner-slide-1.jpg');"></figure>
               </div>
-  
+
               <div class="carousel-item ">
-                <figure class="figure-banner" style="background-image: url('web/images/cus/banner-slide-1.jpg');"></figure>
+                <figure class="figure-banner" style="background-image: url('public/web/images/cus/banner-slide-1.jpg');"></figure>
               </div>
-  
+
               <div class="carousel-item ">
-                <figure class="figure-banner" style="background-image: url('web/images/cus/banner-slide-1.jpg');"></figure>
+                <figure class="figure-banner" style="background-image: url('public/web/images/cus/banner-slide-1.jpg');"></figure>
               </div>
             </div>
-  
+
             <!-- Indicators -->
             <ol class="carousel-indicators">
               <li data-target="#banner-Carousel" data-slide-to="0" class="active"></li>
               <li data-target="#banner-Carousel" data-slide-to="1"></li>
               <li data-target="#banner-Carousel" data-slide-to="2"></li>
             </ol>
-  
+
             <!-- Left and right controls -->
             <a class="carousel-control-prev " href="#banner-Carousel" data-slide="prev">
               <i class="fa fa-angle-left" aria-hidden="true"></i>
