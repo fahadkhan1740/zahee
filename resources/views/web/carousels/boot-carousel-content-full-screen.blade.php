34e0b42578307941028a5d3106e61363bd38ccb8
Fahad Khan <fahadkhan1740@outlook.com>
2020-11-23 21:05:17 +0300

    removed public prefix from homepage

--- resources/views/web/carousels/boot-carousel-content-full-screen.blade.php
+++ resources/views/web/carousels/boot-carousel-content-full-screen.blade.php
@@ -14,7 +14,7 @@
                                echo 'carousel-item';
                            } ?>">
                             <figure class="figure-banner"
-                                    style="background-image: url({{ 'public/'.$slide->path }});"></figure>
+                                    style="background-image: url({{ $slide->path }});"></figure>
                         </a>
                     @endforeach
                 </div>
